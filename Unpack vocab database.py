23cfb5d0cd31f4604b1fb09a4e67953b568af9f6
Laura <laura.dekker@students.unibe.ch>
2023-05-17 12:17:50 +0200

    Updated vocab package

--- Unpack vocab database.py
+++ Unpack vocab database.py
@@ -9,13 +9,8 @@ import pandas as pd
 
  # Construct the databases and save the locally to .csv files.
 
-#Maybe idea to ask user for path to directory and then add filename ourselves?
-#ask just for the directory and then concatenats the string with the standard filename we set, user does not have to see this
-filepath_laura_de = 'C:/Users/laura/OneDrive/Documenten/Bern/Universiteit/FS2023/473540_Advanced Python/Project/EN-DE.csv'
-filepath_laura_nl = 'C:/Users/laura/OneDrive/Documenten/Bern/Universiteit/FS2023/473540_Advanced Python/Project/EN-NL.csv'
-
-def german_vocab(filepath):
-   #German vocabulary dataset
+def german_vocab():
+    #German vocabulary dataset
     en_de_dict = {"Hello": "Hallo", "Yes": "Ja", "No": "Nein", "Thank you": "Danke", "Left": "Links", "Right": "Rechts", "Bye": "Tschüss", "School": "Schule", "University": "Universität", "Bioinformatics": "Bioinformatik"}
     en_de_basic = pd.Series(en_de_dict)
     
@@ -38,9 +33,9 @@ def german_vocab(filepath):
     use_de = ["Hallo, wie geht's?", "Ja, genau", "Nein, bitte nicht", "Danke für die Blumen", "Nächste Ausfahrt links", "Nächste Ausfahrt rechts", "Bis bald. Tschüss", "Ich gehe zur Schule", "Ich studiere an der Universität", "Ich studiere Bioinformatik"]
     en_de_data['Usage'] = use_de
     #save locally to .csv file
-    en_de_data.to_csv(filepath)
+    en_de_data.to_csv("~/EN-DE.csv")
     
-def dutch_vocab(filepath):
+def dutch_vocab():
     #Dutch vocabulary dataset
     en_nl_dict = {"Hello": "Hallo", "Yes": "Ja", "No": "Nee", "Thank you": "Bedankt", "Left": "Links", "Right": "Rechts", "Bye": "Doei", "School": "School", "University": "Universiteit", "Bioinformatics": "Bioinformatica"}
     en_nl_basic = pd.Series(en_nl_dict)
@@ -54,7 +49,7 @@ def dutch_vocab(filepath):
     en_nl_data = en_nl_data.rename(columns = {'index':'English'})
     
     #add columns for difficulty, usage and pronounciation
-    #difficulty, order is same as in German so reuse same code
+    diff = ["I","I","I","I","I","I","I","I","II", "III"]
     en_nl_data['Difficulty'] = diff
     #phonetic pronounciation
     phon_nl = ["hɑlo", "ja", "ne", "bədɑŋkt", "lɪŋks", "rɛx(t)s", "duj", "sxol", "ynivɛrsi'tɛit", "bijoɪnfɔrˈmatika"]
@@ -63,5 +58,5 @@ def dutch_vocab(filepath):
     use_nl = ["Hallo, hoe gaat het?", "Ja, ik ben het ermee eens", "Nee, dat hoef ik niet", "Bedankt voor de bloemen", "Volgende afslag naar links", "Ik schrijf met rechts", "Tot de volgende keer. Doei", "Ik zit op school", "Ik studeer aan de universiteit", "Ik studeer Bioinformatica"]
     en_nl_data['Usage'] = use_nl
     #save locally to .csv file
-    en_nl_data.to_csv(filepath)
+    en_nl_data.to_csv("~/EN-NL.csv")
 
