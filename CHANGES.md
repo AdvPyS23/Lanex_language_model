9f3f4f55f78cfdd55f7fc975dfff7d28f7298c3b
corufang51@gmail.com <xiaoyue.deng@students.unibe.ch>
2023-05-17 11:59:21 +0200

    added more words in the database

--- CHANGES.md
+++ CHANGES.md
@@ -6,8 +6,9 @@
 * [x] Decomposition: broke the main code into front end and back end, may seperate function further for readability
 * [x] Abstraction: unecessary information are hidden by calling a user interface. 
 * [ ] Button to sort words by : difficulty, category
-* [ ] Improve UI: have a main screen where it branches to different functions.
+* [-] Improve UI: have a main screen where it branches to different functions.
 * [ ] Try to avoid 'too many statement' issue
+* [ ] Better segmentation of the page: devide into different parts
 * [ ] Let user choose difficulty
 * [ ] Add option for user to initialize a pre-built dataset
 * [ ] Unify the data sctructure of all four built-in language
--- database_generator_french.py
+++ database_generator_french.py
@@ -4,10 +4,19 @@ import pandas as pd
 vocabulary_list = pd.DataFrame(columns=["word", "translation", "pronunciation", "usage", "category", "difficulty"])
 
 # Add some data to the DataFrame (example data)
+# category: singular
 data = [
-    {"word": "apple", "translation": "pomme", "pronunciation": "ˈæpəl", "usage": "I ate an apple today.", "category": "fruits", "difficulty": "easy"},
-    {"word": "house", "translation": "maison", "pronunciation": "haʊs", "usage": "This is my house.", "category": "buildings", "difficulty": "easy"},
-    {"word": "car", "translation": "voiture", "pronunciation": "kɑr", "usage": "I have a red car.", "category": "vehicles", "difficulty": "easy"}
+    {"word": "pomme", "translation": "apple", "pronunciation": "pɔm", "usage": "J'ai mangé une pomme aujourd'hui.", "category": "fruit", "difficulty": "I"},
+    {"word": "maison", "translation": "house", "pronunciation": "mɛ.zɔ̃", "usage": "J'ai une maison.", "category": "building", "difficulty": "I"},
+    {"word": "voiture", "translation": "car", "pronunciation": "vwɑˈtjʊə(ɹ)", "usage": "J'ai une voiture rouge.", "category": "vehnicle", "difficulty": "I"},
+    {"word": "poisson", "translation": "fish", "pronunciation": "pwa.sɔ̃", "usage": "Je n'aime pas manger du poisson.", "category": "vehnicle", "difficulty": "I"},
+    {"word": "banane", "translation": "banana", "pronunciation": "ba.nan", "usage": "J'ai acheté une banane au marché.", "category": "fruit", "difficulty": "I"},
+    {"word": "orange", "translation": "orange", "pronunciation": "ɔ.ʁɑ̃ʒ", "usage": "Elle a pressé une orange pour faire du jus.", "category": "fruit", "difficulty": "I"},
+    {"word": "mangue", "translation": "mango", "pronunciation": "mɑ̃ɡ", "usage": "La mangue est un fruit exotique très savoureux.", "category": "fruit", "difficulty": "III"}
+    {"word": "pêche", "translation": "peach", "pronunciation": "pɛʃ", "usage": "Il a mangé une pêche juteuse.", "category": "fruit", "difficulty": "II"},
+    {"word": "framboise", "translation": "raspberry", "pronunciation": "fʁɑ̃bwaz", "usage": "Elle a préparé une tarte aux framboises.", "category": "fruits", "difficulty": "III"},
+    {"word": "chat", "translation": "cat", "pronunciation": "ʃa", "usage": "Le chat dort sur le canapé.", "category": "animal", "difficulty": "I"},
+    {"word": "livre", "translation": "book", "pronunciation": "livʁ", "usage": "J'ai acheté un nouveau livre à la librairie.", "category": "education", "difficulty": "II"}
 ]
 
 # Convert the data to a DataFrame
@@ -17,5 +26,5 @@ data_df = pd.DataFrame(data)
 vocabulary_list = pd.concat([vocabulary_list, data_df], ignore_index=True)
 
 # Save the DataFrame to a CSV file
-vocabulary_list.to_csv("vocabulary_french.csv", index=False)
+vocabulary_list.to_csv("vocabulary_french.csv", index=False, header=True)
 
